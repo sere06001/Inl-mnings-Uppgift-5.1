bf0486f7efe40e8c348422b5d867a10d8a2d60cd
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 09:48:05 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uupgift5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 5 namn på en rad");
+            string[] namn = Console.ReadLine().Split();
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine(namn[i]); 
+            }
+        }
+    }
+}
